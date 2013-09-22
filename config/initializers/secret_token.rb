37c622f59f77f42abb1eb6d17f55c2b2668c86ed
Ryan <oloagentolo@gmail.com>
2013-09-21 19:35:26 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CandlepowerLED2::Application.config.secret_key_base = '69b9e1d80aac03224f0ced0ab7015da46cc9b0500ce99441716d45f454deb0c9d31397b2b77740ed87a9856576bc3d6c74b175d489aa836788a5d0893c56ca9e'
